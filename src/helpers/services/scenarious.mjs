313648628d10ea9737a26a36a6fab7bcf3b75a7a
Vlad <svlad1999@ukr.net>
2019-07-12 22:56:30 +0300

    Update scenarious.mjs

--- src/helpers/services/scenarious.mjs
+++ src/helpers/services/scenarious.mjs
@@ -1,14 +1,24 @@
-
+import { logger } from '../logger.mjs'
 
 const Factory2 = param => {
-    return async (response, Message) => {
-        await ctxManager.setContext({
-          scope: commands[param],
-          previousScope: ctxManager.getContext('scope'),
-        })
-        response.send(new Message.Text(papyrus[param], menus[param]))
-      }
-    
+  return async (ctx, Markup) => {
+    try {
+      await ctxManager.setContext({
+        scope: commands[param],
+        previousScope: ctxManager.getContext('scope'),
+      })
+      const keyboardTodisplay = keyboard[param] || keyboard.goBack
+      ctx.reply(
+        papyrus[param],
+        Markup.keyboard(keyboardTodisplay)
+          .oneTime()
+          .resize()
+          .extra(),
+      )
+    } catch (e) {
+      logger.error(e)
+    }
+  }
 }
 
 export const scenarious = new Map()
